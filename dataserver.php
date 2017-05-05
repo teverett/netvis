13516be0c1e40932737c4d55d0499b7124ec40d0
Tom Everett <tom@khubla.com>
2017-05-05 16:39:32 -0600

    ff

--- dataserver.php
+++ dataserver.php
@@ -7,6 +7,21 @@ $username = "netvis";
 $password = "netvis";
 $dbname = "netvis";
 
+class Node
+{
+	var label;
+	var id;
+	var title;
+	var color;
+	var size;
+	var attributes;
+}
+
+class Attributes
+{
+	var Weight;
+}
+
 $nodes = array();
 
 // Create connection
@@ -21,12 +36,15 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
 
+	$node = new Node;
 	// build the node
 	$node->label = $row["hostname"];
 	$node->id = $row["id"];
 	$node->title = $row["ip"];
 	$node->color = "rgb(229,164,67)";
 	$node->size = 5.0;
+	
+	$attributes = new Attributes;
 	$attributes->Weight=1.0;
 	$node->attributes = $attributes;
 
