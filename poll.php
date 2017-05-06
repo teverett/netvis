ccb128c6761fb86dc86048280d450d65a8f1f1e3
Tom Everett <tom@khubla.com>
2017-05-05 20:44:09 -0600

    c

--- poll.php
+++ poll.php
@@ -2,43 +2,37 @@
 include("config.php");
 include("ip.php");
 
-function getAllIPs() {
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-
-	// entire list
-	$ips = array();
-
-	// grab all networks
-	$sql = "SELECT * FROM networks";
-	$result = $conn->query($sql);
-
-	if ($result->num_rows > 0) {
-	    // output data of each row
-	    while($row = $result->fetch_assoc()) {
-
-	    	$network = $row["network"];
-	    	$mask = $row["mask"];
-
-	    	$net = $network."/".$mask;
-	    	echo $net."\r\n";
-
-	    	$list = iplist($net);
-	    	array_push($ips, $list);
-	    }
-	} else {
-	    echo "0 results";
-	}
-	$conn->close();
-
-	return $ips;
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-$ips = getAllIPs();
-var_dump($ips);
+// entire list
+$ips = array();
 
+// grab all networks
+$sql = "SELECT * FROM networks";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+
+    	$network = $row["network"];
+    	$mask = $row["mask"];
+
+    	$net = $network."/".$mask;
+    	echo $net."\r\n";
+
+    	$list = iplist($net);
+    	array_push($ips, $list);
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+var_dump($ips);
 ?>
 
