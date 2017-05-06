036c67540d760788682698ed88c3c7e057b95742
Tom Everett <tom@khubla.com>
2017-05-05 20:06:50 -0600

    ff

---
+++ poll.php
@@ -0,0 +1,27 @@
+
+include("config.php");
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// grab all networks
+$sql = "SELECT * FROM network";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+
+    	$network = $rows["network"];
+    	$mask = $rows["mask"];
+
+    	echo $network;
+
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
