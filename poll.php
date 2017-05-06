5c071c017d08efc856cc7d1b24da7451fab3fafd
Tom Everett <tom@khubla.com>
2017-05-05 20:59:35 -0600

    22

--- poll.php
+++ poll.php
@@ -26,7 +26,7 @@ if ($result->num_rows > 0) {
     	echo $net."\r\n";
 
     	$list = iplist($net);
-    	array_push($ips, $list);
+    	array_merge($ips, $list);
     }
 } else {
     echo "0 results";
