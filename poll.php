77b84a8164f6c8ac712167134b146e1d937a2ad5
Tom Everett <tom@khubla.com>
2017-05-05 21:06:45 -0600

    ee

--- poll.php
+++ poll.php
@@ -8,9 +8,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// entire list
-$ips = array();
-
 // grab all networks
 $sql = "SELECT * FROM networks";
 $result = $conn->query($sql);
